94956a616ba4cb38ce66eb38e3e9bb98f1a821b0
f1nik <f1nik.yar@gmail.com>
2023-04-24 22:04:17 +0300

    Создал модели, добавил класс по работе с данными, добавил rest

---
+++ pereval/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
+from .resources import *
+
+# Create your models here.
+
+class PerevalUser(models.Model):
+    # phone = models.CharField(max_length=128, null=False, blank=False,
+    # unique=True)
+    phone = PhoneNumberField(null=False, blank=False, unique=True)
+    email = models.EmailField(null=False, blank=False, unique=True)
+    firstname = models.CharField(max_length=255, default=None)
+    lastname = models.CharField(max_length=255, default=None)
+    middlename = models.CharField(max_length=255, default=None)
+
+
+class Coords(models.Model):
+    add_time = models.DateTimeField(auto_now_add=True)
+    latitude = models.FloatField(default=None, null=True)
+    longitude = models.FloatField(default=None, null=True)
+    height = models.IntegerField(default=None, null=True)
+
+class Image(models.Model):
+    add_time = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    data = models.CharField(max_length=255)
+
+
+class Pereval(models.Model):
+    user = models.ForeignKey(PerevalUser, on_delete=models.CASCADE, null=True)
+    add_time = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=2, choices=STATUSES, default='new')
+    title = models.CharField(max_length=255, default='', help_text='Enter title', null=True, blank=True)
+    other_titles = models.CharField(max_length=255, default='', help_text='Enter other title', null=True, blank=True)
+    beautyTitle = models.CharField(max_length=255, default='', help_text='Enter beauty title', null=True, blank=True)
+    coords = models.ForeignKey(Coords, on_delete=models.CASCADE, null=True)
+    connect = models.CharField(max_length=255, default='', null=True, blank=True)
+    images = models.ManyToManyField(Image, through="PerevalImages")
+    winter = models.CharField(max_length=2, choices=LEVELS, blank=True, default='')
+    summer = models.CharField(max_length=2, choices=LEVELS, blank=True, default='')
+    autumn = models.CharField(max_length=2, choices=LEVELS, blank=True, default='')
+    spring = models.CharField(max_length=2, choices=LEVELS, blank=True, default='')
+
+class PerevalImages(models.Model):
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    pereval = models.ForeignKey(Pereval, on_delete=models.CASCADE)
---
+++ pereval/serializers.py
@@ -0,0 +1,59 @@
+from pereval.models import *
+from rest_framework import serializers
+
+
+class CoordsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Coords
+        fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PerevalUser
+        fields = '__all__'
+
+
+class ImageSerialize(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = ['data', 'title']
+
+
+class PerevalSerializer(serializers.ModelSerializer):
+    coords = CoordsSerializer()
+    user = UserSerializer()
+    images = ImageSerialize(many=True)
+
+    class Meta:
+        model = Pereval
+        fields = [
+            'title',
+            'other_titles',
+            'beautyTitle',
+            'connect',
+            'status',
+            'user',
+            'coords',
+            'images',
+            'winter',
+            'spring',
+            'summer',
+            'autumn']
+
+    def create(self, validated_data):
+        coords_data = validated_data.pop('coords')
+        user_data = validated_data.pop('user')
+        images_data = validated_data.pop('images')
+
+        coords = Coords.objects.create(**coords_data)
+        user = PerevalUser.objects.create(**user_data)
+        pereval = Pereval.objects.create(coords=coords, user=user, **validated_data)
+
+        for image_data in images_data:
+            image = Image.objects.create(**image_data)
+            PerevalImages.objects.create(pereval=pereval, image=image)
+
+        pereval.save()
+
+        return pereval
