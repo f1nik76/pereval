ecb7bbc09463ee617e2a0fbb7c223cdd368ac6b8
f1nik <f1nik.yar@gmail.com>
2023-05-12 23:01:43 +0300

    Добавил Swagger, внес изменения в urls.py

--- pereval_project/urls.py
+++ pereval_project/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+from rest_framework.schemas import get_schema_view
+
 from pereval import views
 from rest_framework_nested import routers
 
@@ -9,5 +12,7 @@ router.register(r'submitData', views.PerevalViewset, basename='submitData')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api_schema/', get_schema_view(title='API Schema', description='REST API'), name='api_schema'),
+    path('docs/', TemplateView.as_view(template_name='docs.html', extra_context={'schema_url':'api_schema'}), name='swagger-ui'),
 ]
